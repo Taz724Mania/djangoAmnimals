28068a7988e3577f8fcc11f26f702dfa3b41fd40
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-03-27 19:20:10 -0400

    deployment

---
+++ src/pages/Index.jsx
@@ -0,0 +1,23 @@
+import Pet from "../components/Pet"
+import { Form, useLoaderData } from "react-router-dom"
+
+export default function Index(props) {
+    const allPets = useLoaderData()
+
+    return (
+        <>
+            <h1>Add A Turtle</h1>
+            <Form>
+                <label htmlFor="name">
+                    <input type="text" name="name" id="name" />
+                </label>
+                <label htmlFor="age">
+                    <input type="number" name="age" id="age" />
+                </label>
+                <button>Add New Turtle</button>
+            </Form>
+            <hr />
+            {allPets.map((pet, i) => <Pet pet={pet} key={i}/>)}
+        </>
+    )
+}
